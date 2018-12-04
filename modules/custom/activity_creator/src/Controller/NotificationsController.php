a9b33daeae150061ccf1e0fc8e282c6772438305
Truls S. Yggeseth <truls@signwo.com>
2018-12-04 20:07:18 +0100

    Issue #3015936 by truls1502, eiriksm: Fixed Dependency Injection for modules/custom/activity_creator/src/Controller/NotificationsController.php

--- modules/custom/activity_creator/src/Controller/NotificationsController.php
+++ modules/custom/activity_creator/src/Controller/NotificationsController.php
@@ -2,22 +2,45 @@
 
 namespace Drupal\activity_creator\Controller;
 
+use Drupal\activity_creator\ActivityNotifications;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Notifications controller.
  */
 class NotificationsController extends ControllerBase {
 
+  protected $activityNotifications;
+
+  /**
+   * NotificationsController constructor.
+   */
+  public function __construct(AccountProxyInterface $current_user, ActivityNotifications $activity_notifications) {
+    $this->currentUser = $current_user;
+    $this->activityNotifications = $activity_notifications;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user'),
+      $container->get('activity_creator.activity_notifications')
+    );
+  }
+
   /**
    * Ajax callback to mark notifications as read.
    */
   public function readNotificationCallback() {
-    $account = \Drupal::currentUser();
+    $account = $this->currentUser;
 
-    // @todo: Add dependency injection.
-    $activity_notifications = \Drupal::service('activity_creator.activity_notifications');
+    // Dependency injection.
+    $activity_notifications = $this->activityNotifications;
     $remaining_notifications = $activity_notifications->markAllNotificationsAsSeen($account);
 
     // Create AJAX Response object.
